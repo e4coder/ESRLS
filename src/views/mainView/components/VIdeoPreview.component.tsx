f731a193a16cac85fa9c8f87ddb9c2cabdc8d24c
e4coder <64578468+e4coder@users.noreply.github.com>
2021-06-02 12:11:08 +0500

    linked styles for videopreview

--- src/views/mainView/components/VIdeoPreview.component.tsx
+++ src/views/mainView/components/VIdeoPreview.component.tsx
@@ -1,12 +1,14 @@
 import React from 'react';
 
+import Styles from './VideoPreview.module.css';
+
 export default function VideoPreview() {
   return (
     <div className="windowSection">
       <div className="windowHeader">
         <div className="windowTitle">Preview Window</div>
       </div>
-      <div className="windowContent">
+      <div className={`windowContent ${Styles.test}`}>
         <h1>Content</h1>
       </div>
     </div>
